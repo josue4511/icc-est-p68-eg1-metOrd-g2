eafa2a2063b9892f5b789af16b65e8f2aa68f030
PabloTorres <ptorresp@ups.edu.ec>
2026-05-18 15:24:19 -0500

    Grupo 2

--- src/controllers/MovieController.java
+++ src/controllers/MovieController.java
@@ -1,17 +1,14 @@
 package controllers;
 
-import models.Movie;
-
 public class MovieController {
 
     /**
      * Método que debe ser implementado
-     * Debe ordenar el arreglo de películas por título en orden ascendente.
+     * Debe ordenar el arreglo de películas por título en orden descendente.
+     * 
+     * Metodo: sortByTitle
      * 
      * @param movies Arreglo de películas a ordenar
      */
-    public void sortByTitle(Movie[] movies) {
-
-    }
 
 }
